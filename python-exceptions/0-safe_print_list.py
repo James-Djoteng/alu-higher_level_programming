b922c62eed9a35435ece4ae2498b81af0a94ab81
Pro <pro@Macbook-pro-de-pro.local>
2022-07-21 15:53:09 +0300

    e

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x= 0):
     count = 0
     try:
@@ -12,4 +11,3 @@ def safe_print_list(my_list=[], x= 0):
     return count
 
 
-safe_print_list([2,3], x= 30 )
