fff9ce82090952db27bb1e85515e612a7072e37d
Pro <pro@Macbook-pro-de-pro.local>
2022-08-03 18:21:47 +0300

    e

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,14 +2,15 @@
 """
 	Sends a POST request to the URL passed into script	
 """
+
 import urllib.request
 import urllib.parse 
 from sys import argv
-if __name__ == __main__:
+if __name__ == "__main__":
 	data ={'email': argv[2]}
 	data = urllib.parse.encode(data)
 	data = data.encode('ascii')
 	url = argv[1]
 	req = urllib.request.Request(url, data)
 	with urllib.request.urlopen(req) as response:
-		print(response.read().decode(encoding='utf-8'))
+		print(response.read().decode(encoding="utf-8"))
