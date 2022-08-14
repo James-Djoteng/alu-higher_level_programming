9027cac3224cf02d5103938c35c04f7385f1b2f6
Pro <pro@Macbook-pro-de-pro.local>
2022-08-14 19:04:30 +0300

    e

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" add_integer module """
+def add_integer(a, b=98):
+    """ Function that adds two integers.
+    Args:
+        a (int): first number
+        b (int): second number
+    Returns:
+        a + b
+    """
+    if a:
+        if type(a) == float:
+            a = int(a)
+        if type(b) == float:
+            b = int(b)
+        if type(a) != int and type(a) != float:
+            raise TypeError("a must be an integer")
+        if type(b) != int and type(b) != float:
+            raise TypeError("b must be an integer")
+        return a + b
+    else:
+        raise TypeError("a must be an integer")
---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" matrix_divided module """
+
+
+def matrix_divided(matrix, div):
+
+    """ Divides all elements of matrix
+    Args:
+        matrix (list): list of list of integers
+        div (int): integer to divide matrix by
+    Returns:
+        a new list with the divided matrix.
+    """
+    result = 0
+    nuevo = []
+    error1 = "matrix must be a matrix (list of lists) of integers/floats"
+    error2 = "Each row of the matrix must have the same size"
+    if matrix == [] or matrix == [[]]:
+        raise TypeError(error1)
+    if type(matrix) != list:
+        raise TypeError(error1)
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not div:
+        raise TypeError("div must be a number")
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+
+    if matrix and matrix[0]:
+        for row in matrix:
+            temp = []
+            lenirow = matrix[0]
+            if len(matrix) != 2:
+                raise TypeError(error1)
+            if len(row) == 0:
+                raise TypeError(error1)
+            if len(lenirow) != len(row):
+                raise TypeError(error2)
+            for j in row:
+                if type(j) != int and type(j) != float:
+                    raise TypeError(error1)
+                result = j / div
+                result = round(result, 2)
+                temp.append(result)
+            nuevo.append(temp)
+        return nuevo
+    else:
+        raise TypeError(error1)
