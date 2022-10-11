9adc5b598ee696d997c02edc7c712cf1fb4cb521
Pro <pro@Macbook-pro-de-pro.local>
2022-10-12 00:49:54 +0200

    e

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const MyVar = process.argv[2];
 if (isNaN(MyVar) === true) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + MyVar);
+  console.log('My number: ' + parseInt(MyVar));
 }
