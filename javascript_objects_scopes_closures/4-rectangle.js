5044ad27f6499d40d4fe4e836a654296260a7bac
Pro <pro@Macbook-pro-de-pro.local>
2022-10-12 18:20:16 +0200

    e

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,30 +1,27 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-  
-    print () {
-      let A = '';
-      for (let i = 0; i < this.width; i++) {
-        A += 'X';
-      }
-      for (let i = 0; i < this.height; i++) {
-        console.log(A);
-      }
+  }
+  print() {
+    let A = '';
+    for (let i = 0; i < this.width; i++) {
+      A += 'X';
     }
-  
-    rotate () {
-      const aux = this.height;
-      this.height = this.width;
-      this.width = aux;
+    for (let i = 0; i < this.height; i++) {
+      console.log(A);
     }
-  
-    double () {
-      this.width = this.width * 2;
-      this.height = this.height * 2;
-    }
-  };
+  }
+  rotate() {
+    const aux = this.height;
+    this.height = this.width;
+    this.width = aux;
+  }
+  double() {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
